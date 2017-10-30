17f2f3bc6aee70729a02cc8bd7038adc922a457e
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-10-30 16:27:33 +0300

    l28 installed webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+  Capybara.javascript_driver = :webkit
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
