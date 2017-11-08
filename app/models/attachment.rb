abfdf13b27b5e4bd68811766699d568f58602522
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-11-08 18:35:23 +0300

    l29 add attachments to the answers

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ApplicationRecord
   mount_uploader :file, FileUploader
 
-  belongs_to :attachmentable, polymorphic: true
+  belongs_to :attachmentable, polymorphic: true, optional: true
 end
