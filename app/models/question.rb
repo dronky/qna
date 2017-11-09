14a1732b5b1e979f370790e6d38b4f1c8e3e2d5f
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-11-09 18:04:01 +0300

    l29 fixed deleting a file from question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   has_many :answers
   belongs_to :user
   has_many :attachments, as: :attachmentable
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
   validates :title, :body, presence: true
 end
