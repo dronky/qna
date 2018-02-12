9c9aa59efd3b82c9113c87b3276d16ab83b471d5
Andrey Makarenko <dronskiy95@yandex.ru>
2018-02-12 15:03:42 -0500

    seeds task

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,11 @@ namespace :deploy do
     end
   end
 
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
   after :publishing, :restart
 end
 
